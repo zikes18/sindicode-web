e066fcc778a591512219c0a6d16d56d835c1b5e8
zikes18 <josias_zklima@hotmail.com>
2025-10-29 11:58:13 -0300

    adicionando dados na aba views e chamando no html

--- noticias/views.py
+++ noticias/views.py
@@ -6,4 +6,10 @@ from django.http import HttpResponse
 # se def fora classe = funçao
 def index(request):
     #return HttpResponse("<h1>Alô Django 2005</h1>")
-    return render(request, 'noticias/index.html')
+    #definindo um mock com dict (dicionário python)
+    dados ={
+        1:{"titulo":"mulheres dev","conteudo":"mulheres programadores em python","data_publicacao":"29/10/2025"},
+        2:{"titulo":"programadores kids","conteudo":"programadores em python no dia das crianças","data_publicacao":"12/10/2025"},
+        3:{"titulo":"Josias novo presidente","conteudo":"Josias é nosso novo presidente","data_publicacao":"06/06/2006"},
+    }
+    return render(request, 'noticias/index.html', {'cards': dados})
