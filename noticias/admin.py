f96004fd53323c1312dcd31e88a5d4e162d658a3
zikes18 <josias_zklima@hotmail.com>
2025-11-11 08:31:28 -0300

    add noticia

--- noticias/admin.py
+++ noticias/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from noticias.models import Categoria, Autor
+from noticias.models import Categoria, Autor, Noticia
 
 admin.site.register(Categoria)
 admin.site.register(Autor)
+admin.site.register(Noticia)
 # Register your models here.
--- noticias/models.py
+++ noticias/models.py
@@ -11,6 +11,18 @@ class Autor(models.Model):
     perfil = models.TextField(null=False, blank=False)
     def __str__(self):
         return self.nome+self.perfil
+
+class Noticia(models.Model):
+    titulo = models.CharField(max_length=90, null=False, blank=False)
+    conteudo = models.TextField(null=False, blank=False)
+    data_publicacao = models.DateTimeField(null=False, blank=False)
+    destaque = models.CharField(max_length=5 , choices=[('0','0'),('1','1'),('2','2'),('3','3'),('4','4')], default='4')
+    foto = models.CharField(max_length=60, null=False, blank=False)
+    #Relacionamento N-1(Muitas Noticias para um Autor)
+    autor = models.ForeignKey(Autor, on_delete=models.CASCADE, related_name='noticias_autor', null=False)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, related_name='noticias_categoria', null=False)
+    def __str__(self):
+        return self.titulo
 # Create your models here.
 #python manage.py makemigrations
 #python manage.py migrate
