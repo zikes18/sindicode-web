77092b7ed3af9322bb844cdba1c7bd55b173c93f
zikes18 <josias_zklima@hotmail.com>
2025-12-02 12:15:14 -0300

    forms com login

--- associados/views.py
+++ associados/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from associados.forms import AssociadoForm, LoginForm
 
@@ -11,7 +12,25 @@ def cadastro(request):
     form = AssociadoForm()
     if request.method == 'POST':
         form = AssociadoForm(request.POST)
-        if form['senha_1'].value() != form['senha_2'].value():
-            return redirect('cadastro')
+        if form.is_valid():
+            if form['senha_1'].value() != form['senha_2'].value():
+                return redirect('cadastro')
+            nome_completo = form['nome_completo'].value()
+            nome_social = form['nome_social'].value()
+            cpf = form['cpf'].value()
+            rg = form['rg'].value()
+            email = form['email'].value()
+            identidade_genero = form['identidade_genero'].value()
+            senha = form['senha_1'].value()
+            if User.objects.filter(username=nome_completo).exists():
+                return redirect('cadastro')
+            associado = User.objects.create_user(
+                username = nome_completo,
+                email = email,
+                password = senha
+            )
+            associado.save()
+            return redirect('login')
+
     return render(request, 'associados/cadastro.html',{'form': form})
 
